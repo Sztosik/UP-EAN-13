f771da79ac1ab1b331c71bc22a7f3b45462db595
Jędrzej Stasik <jedrzej.stasik@gmail.com>
2023-11-30 13:15:28 +0100

    feat: print country name from barcode

--- src/read_code.py
+++ src/read_code.py
@@ -1,3 +1,13 @@
+from .country_codes import country_codes
+
 while True:
-    kod = input("Skanuj kod: ")
-    print(f"Zeskanowany kod: {kod}")
+    code = input("Skanuj kod: ")
+
+    country_code = str(code[:3])
+    print(f"Kod kraju: {country_code}")
+
+    try:
+        print(f"Kraj: {country_codes[country_code]}\n")
+
+    except:
+        print(f"Kraj: niezidentyfikowany\n")
